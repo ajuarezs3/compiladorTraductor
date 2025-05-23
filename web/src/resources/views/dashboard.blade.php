7980146c293e97399ed569df081d49fa301b7470
AlexisJ <alexiskm2017@gmail.com>
2025-05-23 01:30:03 -0600

    add correct blaed

--- web/src/resources/views/dashboard.blade.php
+++ web/src/resources/views/dashboard.blade.php
@@ -9,9 +9,6 @@
         <div class="max-w-7xl mx-auto sm:px-12 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @if (session('success'))
-                        <div style="color: green;">{{ session('success') }}</div>
-                    @endif
 
                         <form action="{{ route('submit.code') }}" method="POST">
                             @csrf
@@ -34,8 +31,28 @@
                             </button>
                         </form>
 
+                    @if (session('success') || session('error'))
+                        <div class="mt-4 text-center">
+                            @if (session('success'))
+                                <div class="inline-block bg-green-100 border border-green-400 text-green-700 px-4 py-2 rounded">
+                                    {{ session('success') }}
+                                </div>
+                            @endif
+
+                            @if (session('error'))
+                                <div class="inline-block bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded">
+                                    {{ session('error') }}
+                                </div>
+                            @endif
+                        </div>
+                    @endif
                 </div>
+
+
             </div>
+
         </div>
     </div>
+
+
 </x-app-layout>
