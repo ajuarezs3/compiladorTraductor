3c2e0092ab248a69e36b6ad0c335c6f0a337ac66
AlexisJ <alexiskm2017@gmail.com>
2025-05-22 23:50:59 -0600

    add generate system in and pom package

--- analizador/src/main/java/org/example/Main.java
+++ analizador/src/main/java/org/example/Main.java
@@ -1,27 +1,23 @@
 package org.example;
 
 import java.io.FileReader;
+import java.io.InputStreamReader;
+
 import java_cup.runtime.Symbol;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            // Cambia "entrada.ss" por el nombre del archivo que quieras analizar
-            FileReader archivo = new FileReader("entrada.ss");
-
-            // Instancia del analizador léxico
-            AnalizadorLexico lexer = new AnalizadorLexico(archivo);
-
-            // Instancia del parser (CUP)
+            // Leer desde stdin (entrada estándar)
+            AnalizadorLexico lexer = new AnalizadorLexico(new InputStreamReader(System.in));
             parser parser = new parser(lexer);
-
-            // Llamada al método parse() para procesar el archivo
             Symbol resultado = parser.parse();
 
-            System.out.println("✅ El código es sintácticamente válido.");
+            System.out.println("El código es sintácticamente válido.");
+            System.exit(0);
         } catch (Exception e) {
-            System.err.println(e.getMessage());
-//            e.printStackTrace();
+            System.err.println("Error de análisis: " + e.getMessage());
+            System.exit(400);
         }
     }
 }
